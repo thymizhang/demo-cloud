8928125d4a7493306cad0b05e8b10591d29d982b
thymi <dev@pmeasy.cn>
2020-09-16 09:59:28 +0800

    Feign调用说明更新

--- service-project/src/main/java/com/demo/project/feign/CompanyFeign.java
+++ service-project/src/main/java/com/demo/project/feign/CompanyFeign.java
@@ -4,6 +4,8 @@ import com.demo.common.service.CompanyService;
 import org.springframework.cloud.openfeign.FeignClient;
 
 /**
+ * 正确的feign使用方式, 通过接口给调用方和被调用方同时使用, 保证一致性
+ *
  * @Author thymi
  * @Date 2020/5/25
  */
